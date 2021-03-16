2ad1223481a47e39c0c466030c033b100e825931
unknown <arnoldsb3@gmail.com>
2021-03-16 16:28:57 +0200

    Piektais uzdevums

--- 05-toggle-elements-of-dom-path/QUESTIONS.md
+++ 05-toggle-elements-of-dom-path/QUESTIONS.md
@@ -7,3 +7,4 @@
 > Describe the contract you used for finding the movies to toggle in the DOM. How is this function different from other functions that find elements in the DOM?
 
 Your reply here...
+Es izmantoju querySelectorAll, kas paņem un saglabā visus klases elementus, nevis tikai vienu, ar if, else un for cikliem pārveidoju 4 uzdevuma JS, pievienojot tam querySelectorAll funkciju.
--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,23 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    const toggleButton = document.getElementById('toggle_button')
+
+    toggleButton.addEventListener('click', function(e){
+        var toggleMe = document.querySelectorAll(".second_five li"); 
+        if(toggleMe[0].style.display == 'none'){
+            for (i = 0; i < toggleMe.length; i++) {
+                toggleMe[i].style.display = "list-item";
+            }
+        }
+        else{
+            for (i = 0; i < toggleMe.length; i++) {
+                toggleMe[i].style.display = "none";
+                
+            }
+        }
+            
+        
+        
+    });
+    
+})
