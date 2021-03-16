219bd6bb64d001a2ed6e947547882712f8f57d66
unknown <arnoldsb3@gmail.com>
2021-03-16 16:03:36 +0200

    Ceturtais uzdevums

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,23 @@
 // Your JavaScript goes here.
+window.addEventListener("load", function(){
+    const toggleButton = document.getElementById('button')
+
+    toggleButton.addEventListener('click', function(e){
+        var toggleMe = document.getElementsByClassName('toggle_me')
+        if(toggleMe[0].style.display == 'none'){
+            for (i = 0; i < toggleMe.length; i++) {
+                toggleMe[i].style.display = "block";
+            }
+        }
+        else{
+            for (i = 0; i < toggleMe.length; i++) {
+                toggleMe[i].style.display = "none";
+                
+            }
+        }
+            
+        
+        
+    });
+    
+})
