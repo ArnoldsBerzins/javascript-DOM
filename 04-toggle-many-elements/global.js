53925a3eb73c3eaa8a4260c63e658539aac22215
ArnoldsBerzins <arnoldsb3@gmail.com>
2021-03-23 16:01:44 +0200

    Efektivakais variants

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,23 +1,15 @@
 // Your JavaScript goes here.
 window.addEventListener("load", function(){
-    const toggleButton = document.getElementById('button')
-
-    toggleButton.addEventListener('click', function(e){
-        var toggleMe = document.getElementsByClassName('toggle_me')
-        if(toggleMe[0].style.display == 'none'){
-            for (i = 0; i < toggleMe.length; i++) {
-                toggleMe[i].style.display = "block";
-            }
+    var button = document.getElementById('button');
+    button.addEventListener("click", function() {
+      var divs_to_toggle = document.getElementsByClassName('toggle_me');
+      for (var i = 0; i < divs_to_toggle.length; i++) {
+        if (divs_to_toggle[i].style.display === "none") {
+          divs_to_toggle[i].style.display = "block";
         }
-        else{
-            for (i = 0; i < toggleMe.length; i++) {
-                toggleMe[i].style.display = "none";
-                
-            }
+        else {
+          divs_to_toggle[i].style.display = "none";
         }
-            
-        
-        
+      }
     });
-    
-})
+  });
